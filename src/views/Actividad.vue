8a4811d2040045c0d71abb47dfd4d3362828551a
jorozcop <maechavarriaor@gmail.com>
2023-10-13 12:03:33 -0500

    ajustes
    ajustes

--- src/views/Actividad.vue
+++ src/views/Actividad.vue
@@ -7,15 +7,15 @@
       .col-6.col-md-4.col-lg-3.mb-4.mb-md-0
         .tarjeta.h-100.d-flex.align-items-center.p-4
           figure
-            img(src="@/assets/template/falso-verdadero.svg", alt="Texto que describa la imagen")
+            img(src="@/assets/template/opcion-multiple.svg", alt="Texto que describa la imagen")
       .col-12.col-md-8.col-lg-9
-        .titulo-segundo.color-acento-contenido
-          h2 Cuestionario conceptual
+        .titulo-segundo.color-acento-botones
+          h2 Recursos de la red de datos en las organizaciones
         p.mb-4 Evaluar el nivel de comprensión y aplicabilidad de las estrategias de Recursos para determinar la gestión de las redes de datos en las Organizaciones.
         .tarjeta.actividad.p-3
           .row.justify-content-around.align-items-center            
             .col-sm.mb-3.mb-sm-0
-              p.fw-bold.mb-0 Cuestionario
+              p.fw-bold.mb-0 Cuestionario conceptual
             .col-auto
               a.boton.boton--b(:href="obtenerLink('/actividades/index.html')" target="_blank")
                 span Realizar
